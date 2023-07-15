b974d49810b00352adcd4dfaa02d768491c6eebc
Vladimir Fedorov <nightsense@mail.ru>
2023-07-15 23:36:57 +0300

    hhh

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                    [{text: 'Заполнить 11 форму', web_app: {url: webAppUrl + '/form'}}]
                 ]
             }
         })
